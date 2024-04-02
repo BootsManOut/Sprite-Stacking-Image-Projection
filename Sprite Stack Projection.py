b270b24702a81a039e5e57648d4b7178a279e0cd
BootsManOut <63544082+BootsManOut@users.noreply.github.com>
2024-04-02 15:00:28 +0200

    Add files via upload

---
+++ Sprite Stack Projection.py
@@ -0,0 +1,25 @@
+# Project image unto sprite stack:
+from PIL import Image
+
+# Open image and access it's pixel data and size:
+image = Image.open("image.png")
+image_pixels = image.load()
+size = width, height = image.size
+name_digit_length = len(str(height))
+
+for y in range(height):
+    # Open the model image of the sprite stack and access it's data:
+    # Model image should have the same width as the original image:
+    output_layer = Image.open("sprite stack model 2.png")
+    output_layer = output_layer.convert("RGBA")
+    output_pixels = output_layer.load()
+    output_size = output_width, output_height = output_layer.size
+
+    # Draw the image unto the opaque pixels:
+    for x in range(width):
+        for y2 in range(output_height):
+            if not output_pixels[x, y2][3] == 0:
+                output_pixels[x, y2] = image_pixels[x, y]
+    # Place the proper amount of 0's in front of the digit:
+    digit = y if len(str(y)) == name_digit_length else "0"*(name_digit_length - len(str(y))) + str(y)
+    output_layer.save(f"Sprite Stack Frames/sprite stack {digit}.png")
